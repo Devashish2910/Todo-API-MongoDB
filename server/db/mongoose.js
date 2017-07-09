18cab53cd7a8343be326a311316bb865405f8d1d
Devashish2910 <dgpatel2910@gmail.com>
2017-07-09 07:24:40 -0500

    Environment setup for Heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 // connect database
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://devashish:devashish2910@ds147882.mlab.com:47882/todoapp' || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {
   mongoose
